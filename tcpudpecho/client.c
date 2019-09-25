40065249f2d5e4595a5bde8be8c6d88999e9f964
Ravana99 <tomas.lopes@tecnico.ulisboa.pt>
2019-09-25 17:38:03 +0100

    TCP/UDP echo server (bug: recvfrom fails half the time)
    TCP/UDP echo server (bug: recvfrom fails half the time)

---
+++ tcpudpecho/client.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define PORT "58001"
+
+int main() {
+    struct addrinfo hintstcp, hintsudp, *restcp, *resudp;
+    ssize_t n, nbytes, nleft, nwritten, nread;
+    int tcpfd, udpfd, errcode;
+    struct sigaction act;
+    struct sockaddr_in addr;
+    socklen_t addrlen;
+    char hostname[128], *ptr, buffer[128], received[128];
+
+    memset(buffer, 0, sizeof buffer);
+    memset(received, 0, sizeof received);
+
+    memset(&hintstcp, 0, sizeof hintstcp);
+    hintstcp.ai_family = AF_INET;
+    hintstcp.ai_socktype = SOCK_STREAM;
+    hintstcp.ai_flags = AI_NUMERICSERV;
+    memset(&hintsudp, 0, sizeof hintsudp);
+    hintsudp.ai_family = AF_INET;
+    hintsudp.ai_socktype = SOCK_DGRAM;
+    hintsudp.ai_flags = AI_NUMERICSERV;
+
+    memset(&act, 0, sizeof act);
+    act.sa_handler = SIG_IGN;
+    if (sigaction(SIGPIPE, &act, NULL) == -1) exit(1);
+    
+    if (gethostname(hostname, 128) == -1) exit(1);
+
+    if ((errcode = getaddrinfo(hostname, PORT, &hintstcp, &restcp)) != 0) exit(1);
+    if ((errcode = getaddrinfo(hostname, PORT, &hintsudp, &resudp)) != 0) exit(1);
+
+    // TCP sending
+
+    if ((tcpfd = socket(restcp->ai_family, restcp->ai_socktype, restcp->ai_protocol)) == -1) exit(1);
+
+    if ((errcode = connect(tcpfd, restcp->ai_addr, restcp->ai_addrlen)) == -1) exit(1);
+
+    ptr = strcpy(buffer, "Hello!\n");
+    nbytes = 7;
+
+    nleft = nbytes;
+    while (nleft > 0) {
+        if ((nwritten = write(tcpfd, ptr, nleft)) <= 0) exit(1);
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+
+    ptr = received;
+    nleft = nbytes;
+    while (nleft > 0) {
+        if ((nread = read(tcpfd, ptr, nleft)) == -1) exit(1);
+        else if (nread == 0) break;
+        nleft -= nread;
+        ptr += nread;
+    }
+
+    nread = nbytes-nleft;
+    write(1, "echo from tcp: ", 15);
+    write(1, received, nread);
+
+    close(tcpfd);
+
+    memset(buffer, 0, sizeof buffer);
+
+    // UDP sending
+
+    if ((udpfd = socket(resudp->ai_family, resudp->ai_socktype, resudp->ai_protocol)) == -1) exit(1);
+
+    if ((n = sendto(udpfd, "Hi!\n", 4, 0, resudp->ai_addr, resudp->ai_addrlen)) == -1) exit(1);
+
+    if ((n = recvfrom(udpfd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen)) == -1) { write(1, "recvfrom: ", 10); write(1, strerror(errno), strlen(strerror(errno))); exit(1); }
+    // BUG: RECVFROM FAILS ABOUT HALF THE TIME
+
+    write(1, "echo from udp: ", 15);
+    write(1, buffer, n);
+
+    freeaddrinfo(restcp);
+    freeaddrinfo(resudp);
+    close(udpfd);
+
+    exit(0);
+}
+
+
+/*
+
+int main() {
+    struct addrinfo hints, *res;
+    int fd, errcode;
+    ssize_t n;
+    struct sockaddr_in addr;
+    socklen_t addrlen;
+    char hostname[128], buffer[128];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_NUMERICSERV;
+   
+    
+    if (gethostname(hostname, 128) == -1) exit(1);
+
+    if ((errcode = getaddrinfo(hostname, "58001", &hints, &res)) != 0) exit(1);
+
+    if ((fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) exit(1);
+
+    if ((n = sendto(fd, "Hello!\n", 7, 0, res->ai_addr, res->ai_addrlen)) == -1) exit(1);
+
+    addrlen = sizeof(addr);
+    if ((n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen)) == -1) exit(1);
+
+    write(1, "echo: ", 6);
+    write(1, buffer, n);
+
+    freeaddrinfo(res);
+    close(fd);
+
+    exit(0);
+}
+
+*/
---
+++ tcpudpecho/server.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#define PORT "58001"
+#define MAXBUFFER 512
+#define max(A, B) ((A) >= (B) ? (A) : (B))
+
+void exit1(char* s) {
+    fprintf(stderr, "%s: %s\n", s, strerror(errno));
+    exit(1);
+}
+
+int main() {
+
+    int listenfd, connfd, udpfd, nready, maxfdp1, errcode, n;
+    char buffer[MAXBUFFER];
+    fd_set rset;
+    struct sockaddr_in cliaddr;
+    struct addrinfo hintstcp, hintsudp, *restcp, *resudp;
+    socklen_t len;
+    ssize_t nw;
+    char *ptr;
+    struct sigaction act, act2;
+    pid_t childpid;
+    
+    memset(buffer, 0, sizeof buffer);
+
+    memset(&hintstcp, 0, sizeof hintstcp);
+    hintstcp.ai_family = AF_INET;
+    hintstcp.ai_socktype = SOCK_STREAM;
+    hintstcp.ai_flags = AI_PASSIVE|AI_NUMERICSERV;
+    memset(&hintsudp, 0, sizeof hintsudp);
+    hintsudp.ai_family = AF_INET;
+    hintsudp.ai_socktype = SOCK_DGRAM;
+    hintsudp.ai_flags = AI_PASSIVE|AI_NUMERICSERV;
+
+    memset(&act, 0, sizeof act);
+    act.sa_handler = SIG_IGN;
+    if (sigaction(SIGPIPE, &act, NULL) == -1) exit1("sigaction");
+    memset(&act2, 0, sizeof act);
+    act2.sa_handler = SIG_IGN;
+    if (sigaction(SIGCHLD, &act2, NULL) == -1) exit1("sigaction");
+
+    if ((errcode = getaddrinfo(NULL, PORT, &hintstcp, &restcp)) != 0) exit1("getaddrinfo");
+    if ((errcode = getaddrinfo(NULL, PORT, &hintsudp, &resudp)) != 0) exit1("getaddrinfo");
+    
+
+    /* tcp socket */
+    if ((listenfd = socket(restcp->ai_family, restcp->ai_socktype, restcp->ai_protocol)) == -1) exit1("socket");
+/**/if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0) exit1("setsockopt");
+    if (bind(listenfd, restcp->ai_addr, restcp->ai_addrlen) == -1) exit1("bind");
+    if (listen(listenfd, 5) == -1) exit1("listen");
+
+    /* udp socket */
+    if ((udpfd = socket(resudp->ai_family, resudp->ai_socktype, resudp->ai_protocol)) == -1) exit1("socket");
+    if ((errcode = bind(udpfd, resudp->ai_addr, resudp->ai_addrlen)) == -1) exit1("bind");
+/**/if (setsockopt(udpfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0) exit1("setsockopt");
+
+    FD_ZERO(&rset);
+    maxfdp1 = max(listenfd, udpfd) + 1;
+
+    while(1) {
+
+        FD_SET(listenfd, &rset);
+        FD_SET(udpfd, &rset);
+
+        if ((nready = select(maxfdp1 + 1, &rset, (fd_set*) NULL, (fd_set*) NULL, (struct timeval*) NULL)) <= 0) exit1("select");
+
+        if (FD_ISSET(listenfd, &rset)) {
+
+            len = sizeof(cliaddr);
+            if ((connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &len)) == -1) exit1("accept");
+            if ((childpid = fork()) == 0) {
+                close(listenfd);
+                memset(buffer, 0, sizeof buffer);
+                while ((n = read(connfd, buffer, 128)) != 0) {
+                    if (n == -1) exit1("read");
+                    ptr = &buffer[0];
+                    while (n > 0) {
+                        if ((nw = write(connfd, ptr, n)) <= 0) exit1("write");
+                        n -= nw;
+                        ptr += nw;
+                    }
+                }
+                write(1, "received from tcp: ", 19);
+                write(1, buffer, strlen(buffer));
+                close(connfd);
+                exit(0);
+            }
+            else if (childpid == -1) exit1("fork");
+            do errcode = close(connfd); while (errcode == -1 && errno == EINTR);
+            if (errcode == -1) exit1("close");
+
+        }
+
+        if (FD_ISSET(udpfd, &rset)) {
+            len = sizeof(cliaddr);
+            memset(buffer, 0, sizeof buffer);
+            if ((n = recvfrom(udpfd, buffer, sizeof buffer, 0, (struct sockaddr*)&cliaddr, &len)) == -1) exit1("recvfrom");
+            write(1, "received from udp: ", 19);
+            write(1, buffer, strlen(buffer));
+            if ((n = sendto(udpfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&cliaddr, len)) == -1) exit1("sendto");
+        }
+    }   
+
+    freeaddrinfo(restcp);
+    freeaddrinfo(resudp);
+    close(listenfd);
+    close(udpfd);
+
+    exit(0);
+}
