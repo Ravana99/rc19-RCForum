dfd07723e58886c6330456702626c8a43f66b187
Tiago Pires <tiago.silva.pires@tecnico.ulisboa.pt>
2019-10-12 13:56:08 +0100

    WIP TCP

--- client/user.c
+++ client/user.c
@@ -63,17 +63,22 @@ void setHostNameAndPort(int argc, char **argv, char *hostname, char *port)
 void getImageExtension(char *imagefilename, char *iext)
 {
     int i, j = 0, imagefilename_length = strlen(imagefilename);
-    for (i = 0; i < imagefilename_length; i++)
+    for (i = imagefilename_length - 3; i < imagefilename_length; i++)
     {
-        if (imagefilename[i] == '.')
-            break;
-    }
-    for (i++; i < imagefilename_length; i++)
-    {
-        if (j == 3)
-            break;
         iext[j++] = imagefilename[i];
     }
+    iext[j] = '\0';
+}
+
+int getNumberOfDigits(int number)
+{
+    int count = 0;
+    while (number != 0)
+    {
+        number /= 10;
+        ++count;
+    }
+    return count;
 }
 
 int sendUDP(int udpfd, struct addrinfo **resudp, char *message, char *response)
@@ -91,10 +96,10 @@ int sendUDP(int udpfd, struct addrinfo **resudp, char *message, char *response)
     return n;
 }
 
-void sendTCP(struct addrinfo *restcp, char *message, char *response, int message_length)
+void sendTCP(struct addrinfo *restcp, char *message, char *response, long int message_length)
 {
-    ssize_t nbytes = message_length * sizeof(char), nleft, nwritten, nread;
-    char *ptr, answer[2048];
+    ssize_t nbytes = message_length * sizeof(char), nleft, n;
+    char *ptr;
     int tcpfd;
 
     if ((tcpfd = socket(restcp->ai_family, restcp->ai_socktype, restcp->ai_protocol)) == -1)
@@ -109,35 +114,37 @@ void sendTCP(struct addrinfo *restcp, char *message, char *response, int message
     }
 
     nleft = nbytes;
-    //printf("nbytes %zu\n%s\nresponse %s\n", nbytes, message, response);
+    printf("nbytes %zu\n%s\nresponse %s\n", nbytes, message, response);
     ptr = message;
     while (nleft > 0)
     {
-        write(1, "1\n", 2);
-        if ((nwritten = write(tcpfd, ptr, nleft)) <= 0)
+        //write(1, "1\n", 2);
+        if ((n = write(tcpfd, ptr, nleft)) <= 0)
         {
             write(1, "WERROR\n", 7);
             printf("%s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
 
-        nleft -= nwritten;
-        ptr += nwritten;
+        nleft -= n;
+        ptr += n;
     }
     ptr = response;
-    // nleft = nbytes;
     do
     {
-        if ((nread = read(tcpfd, ptr, 1)) <= 0)
+        if ((n = read(tcpfd, response, 1)) == -1)
         {
             write(1, "RERROR\n", 7);
+            printf("%s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
-        ptr += nread;
-    } while (*(ptr - nread) != '\n');
+        else if (n == 0)
+            break;
+        response += n;
+    } while (*(response - n) != '\n');
 
     //nread = nbytes - nleft;
-    printf("nbytes %zu\n%s\n\n", nbytes, message);
+    printf("ANSWER %s\n", response);
     close(tcpfd);
     //return response
 }
@@ -293,20 +300,19 @@ void questionGet(char *message, char *response, struct addrinfo *restcp, int act
             printf("Invalid topic name\n\n");
     }*/
 }
-void questionSubmit(char *inputptr, char *message, char *response, struct addrinfo *restcp, int userid, int active_topic_number, Topic *topic_list)
+void questionSubmit(char *inputptr, char *response, struct addrinfo *restcp, int userid, int active_topic_number, Topic *topic_list)
 {
     FILE *fp;
-    int i, message_length;
-    char question[11], filename[NAME_MAX + 3], imagefilename[NAME_MAX + 3], *ptr, messageTMP[2048], qdata[2048], *idata, iext[4];
-    imagefilename[0] = '\0';
-    sscanf(inputptr, "%s %s %s", question, filename, imagefilename);
-    ssize_t qsize = 0, isize = 0, nbytes = 0, nleft = 0, nwritten = 0, nread = 0;
+    long int message_length, num_of_chars_written;
+    char question[11], filename[NAME_MAX + 3], imagefilename[NAME_MAX + 3],
+        qdata[2048], *idata, iext[4], *tcp_message;
+    ssize_t qsize = 0, isize = 0;
 
+    imagefilename[0] = '\0';
     sscanf(inputptr, "%s %s %s", question, filename, imagefilename);
     //printf("INPUT:%s\nNAMELIMIT:%d\nfilename:%s\nquestion:l%s\nimagefilename:%s\n", inputptr, NAME_MAX, filename, question, imagefilename);
     strcat(filename, ".txt");
     fp = fopen(filename, "r");
-    message_length = sizeof(userid) + strlen(topic_list[active_topic_number].name) + 1 + strlen(question) + 1 + sizeof(qsize) + strlen(qdata) + 1;
     if (fp == NULL)
     {
         printf("Question File Not Found!\n\n");
@@ -318,9 +324,12 @@ void questionSubmit(char *inputptr, char *message, char *response, struct addrin
     fread(qdata, sizeof(char) * qsize, 1, fp);
     strtok(qdata, "\n");
     fclose(fp);
+    message_length = 3 + 1 + 5 + 1 + strlen(topic_list[active_topic_number].name) +
+                     1 + strlen(question) + 1 + getNumberOfDigits(qsize) + 1 +
+                     strlen(qdata) + 1 + 1 + 1 + 1; //last one is for \0
+
     if (imagefilename[0] != '\0')
     {
-        //printf("HERE File Not Found!\n\n");
         fp = fopen(imagefilename, "rb");
         if (fp == NULL)
         {
@@ -333,32 +342,47 @@ void questionSubmit(char *inputptr, char *message, char *response, struct addrin
         fseek(fp, 0L, SEEK_END);
         isize = ftell(fp);
         fseek(fp, 0L, SEEK_SET);
-        idata = malloc(sizeof(char) * (isize + 1));
-        message = realloc(message, sizeof(char) * isize + sizeof(char) * message_length + 15);
-        message_length += 15 + isize;
+        idata = malloc(sizeof(char) * isize + 1);
+
         fread(idata, sizeof(char) * isize, 1, fp);
         fclose(fp);
 
-        /*TESTAR SE IMAGEM ESTA BEM
-        fp = fopen("damn.jpg", "wb");
-        fwrite(idata, sizeof(char) * isize, 1, fp);
-        fclose(fp);
-        */
+        //TESTAR SE IMAGEM ESTA BEM
+        //fp = fopen("damn2.jpg", "wb");
+        //fwrite(idata, sizeof(char) * isize, 1, fp);
+        //fclose(fp);
+
         /*for (int i = 1; i < 61; i++)
         {
             printf("%s\n", topic_list[i].name);
         }*/
-        sprintf(message, "QUS %d %s %s %ld %s 1 %s %ld %s\n", userid, topic_list[active_topic_number].name, question, qsize, qdata, iext, isize, idata);
+
+        message_length += 1 + 3 + 1 + getNumberOfDigits(isize) + 1 + isize + 1;
+        tcp_message = malloc(sizeof(char) * message_length);
+        num_of_chars_written = sprintf(tcp_message, "QUS %d %s %s %ld %s 1 %s %ld ", userid, topic_list[active_topic_number].name, question, qsize, qdata, iext, isize);
+        memcpy(tcp_message + num_of_chars_written,
+               idata, isize);
+        //for (int i = 0; i < (message_length - isize + 100); i++)
+        //{
+        //    printf("%c", tcp_message[i]);
+        //}
+        //printf("\n\n");
+        tcp_message[message_length - 1] = '\n';
+        //tcp_message[message_length - 1] = '\0';
     }
     else
     {
-        message = realloc(message, sizeof(char) * (message_length + 11));
-        message_length += 11;
-        sprintf(message, "QUS %d %s %s %ld %s 0\n", userid, topic_list[active_topic_number].name, question, qsize, qdata);
+        tcp_message = malloc(sizeof(char) * message_length);
+        sprintf(tcp_message, "QUS %d %s %s %ld %s 0\n", userid, topic_list[active_topic_number].name, question, qsize, qdata);
     }
-    sendTCP(restcp, message, response, message_length);
-    printf("RESPONSE %s\n\n", response);
-    //free(idata);
+    //printf("LENGTH: %d %d\n", tcp_message[message_length - 2], tcp_message[message_length - 1]);
+    printf("LENGTH: %ld %ld\n", message_length, strlen(tcp_message));
+
+    printf("MESSAGE: %s\n", tcp_message);
+    sendTCP(restcp, tcp_message, response, message_length);
+
+    printf("%s\n\n", response);
+    free(tcp_message);
 }
 void answerSubmit()
 {
@@ -376,13 +400,12 @@ int main(int argc, char **argv)
 {
     Topic topic_list[99 + 1]; // First entry is left empty to facilitate indexing
     struct addrinfo hintstcp, hintsudp, *restcp, *resudp;
-    ssize_t n, nbytes, nleft, nwritten, nread;
 
-    int udpfd, errcode, active_topic_number = 0, number_of_topics = 0, userid = 0;
+    int udpfd, active_topic_number = 0, number_of_topics = 0, userid = 0;
     struct sigaction act;
 
-    char port[16], hostname[128], buffer[128], input[128], command[128], response[2048], active_question[11];
-    char *inputptr, *ptr, *message;
+    char port[16], hostname[128], buffer[128], input[128], command[128], response[2048];
+    char *inputptr, *message;
 
     //memset(topic_list, 0, sizeof topic_list);
     message = malloc(sizeof(char) * 2048);
@@ -440,17 +463,17 @@ int main(int argc, char **argv)
             topicPropose(inputptr, message, response, udpfd, resudp,
                          userid, &active_topic_number, &number_of_topics, topic_list, buffer);
 
-        else if (!strcmp(command, "question_list"))
+        else if (!strcmp(command, "question_list") || !strcmp(command, "ql"))
             questionList(message, response, udpfd, resudp,
                          active_topic_number, number_of_topics, topic_list);
 
         else if (!strcmp(command, "question_get"))
             questionGet(message, response, restcp, active_topic_number, topic_list, false);
         else if (!strcmp(command, "qg"))
-           questionGet(message, response, restcp, active_topic_number, topic_list, true);
+            questionGet(message, response, restcp, active_topic_number, topic_list, true);
 
         else if (!strcmp(command, "question_submit") || !strcmp(command, "qs"))
-            questionSubmit(inputptr, message, response, restcp, userid, active_topic_number, topic_list);
+            questionSubmit(inputptr, response, restcp, userid, active_topic_number, topic_list);
 
         else if (!strcmp(command, "answer_submit") || !strcmp(command, "as"))
             answerSubmit();
@@ -466,48 +489,3 @@ int main(int argc, char **argv)
         memset(command, 0, sizeof command);
     }
 }
-
-/*
-
-    if ((tcpfd = socket(restcp->ai_family, restcp->ai_socktype, restcp->ai_protocol)) == -1) exit(1);
-
-    if ((errcode = connect(tcpfd, restcp->ai_addr, restcp->ai_addrlen)) == -1) exit(1);
-
-    ptr = strcpy(buffer, "Hello!\n");
-    nbytes = 7;
-
-    nleft = nbytes;
-    while (nleft > 0) {
-        if ((nwritten = write(tcpfd, ptr, nleft)) <= 0) exit(1);
-        nleft -= nwritten;
-        ptr += nwritten;
-    }
-
-    ptr = received;
-    nleft = nbytes;
-    while (nleft > 0) {
-        if ((nread = read(tcpfd, ptr, nleft)) == -1) exit(1);
-        else if (nread == 0) break;
-        nleft -= nread;
-        ptr += nread;
-    }
-
-    nread = nbytes-nleft;
-    write(1, "echo from tcp: ", 15);
-    write(1, received, nread);
-
-    close(tcpfd);
-
-    memset(buffer, 0, sizeof buffer);
-
-    if ((udpfd = socket(resudp->ai_family, resudp->ai_socktype, resudp->ai_protocol)) == -1) exit(1);
-
-    if ((n = sendto(udpfd, "Hi!\n", 4, 0, resudp->ai_addr, resudp->ai_addrlen)) == -1) exit(1);
-
-    addrlen = sizeof(addr);
-    if ((n = recvfrom(udpfd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen)) == -1) exit(1);
-
-    write(1, "echo from udp: ", 15);
-    write(1, buffer, n);
-
-*/
